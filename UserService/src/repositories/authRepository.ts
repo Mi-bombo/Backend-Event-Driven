2e70e908cfc1bc1e2724026ced6fcbbe5d6f07e6
Gerardo Vega <gerardoluisvega@gmail.com>
2025-11-03 16:14:56 -0300

    ,,,

--- UserService/src/repositories/authRepository.ts
+++ UserService/src/repositories/authRepository.ts
@@ -3,33 +3,33 @@ import { IUser } from "../interfaces/IUser";
 import { generalQueryRepository } from "./dbRepository";
 
 export class authRepository {
-    private generalQueryRepo: generalQueryRepository;
+  private generalQueryRepo: generalQueryRepository;
 
-    constructor() {
-        this.generalQueryRepo = new generalQueryRepository();
-    }
+  constructor() {
+    this.generalQueryRepo = new generalQueryRepository();
+  }
 
-    getUserForId = async (id: string): Promise<IUser | null> => {
-        const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [id]);
-        return result.rows[0] || null;
-    };
+  getUserForId = async (id: string): Promise<IUser | null> => {
+    const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [id]);
+    return result.rows[0] || null;
+  };
 
-    getUserForEmail = async (email: string): Promise<IUser | null> => {
-        const result = await pool.query(`SELECT * FROM users WHERE email = $1`, [email]);
-        return result.rows[0] || null;
-    }
+  getUserForEmail = async (email: string): Promise<IUser | null> => {
+    const result = await pool.query(`SELECT * FROM users WHERE email = $1`, [
+      email,
+    ]);
+    return result.rows[0] || null;
+  };
 
-    deleteUserById = async (id: string): Promise<void> => {
-        await this.generalQueryRepo.deleteItemById('users', id);
-    }
+  deleteUserById = async (id: string): Promise<void> => {
+    await this.generalQueryRepo.deleteItemById("users", id);
+  };
 
-    getItemById = async(id:string | number, algo:string): Promise<any> => {
-        return await pool.query(`SELECT * FROM ${algo} WHERE id = $1`, [id]);
-    }
+  getItemById = async (id: string | number, algo: string): Promise<any> => {
+    return await pool.query(`SELECT * FROM ${algo} WHERE id = $1`, [id]);
+  };
 
-    getItemByIdNumber = async(id:number, algo:string): Promise<any> => {
-        return await pool.query(`SELECT * FROM ${algo} WHERE id = $1`, [id]);
-    }
-
+  getItemByIdNumber = async (id: number, algo: string): Promise<any> => {
+    return await pool.query(`SELECT * FROM ${algo} WHERE id = $1`, [id]);
+  };
 }
-
--- UserService/src/services/user.service.ts
+++ UserService/src/services/user.service.ts
@@ -2,67 +2,79 @@ import { IUser } from "../interfaces/IUser";
 import { compare, hash, genSalt } from "bcrypt";
 import { jwtValidator } from "../middlewares/jwtValidator";
 import { pool } from "../db/db";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import { sendEvent } from "../kafka/producer";
 import { authRepository } from "../repositories/authRepository";
 
-
-
-
-
 export class usersService extends authRepository {
-    constructor(){
-        super();
+  constructor() {
+    super();
+  }
+
+  async loginUserService(
+    email: string,
+    password: string
+  ): Promise<Omit<IUser, "password_hash"> & { token?: string }> {
+    if (!email || !password) {
+      throw new Error("Todos los datos son requeridos");
     }
 
-    async loginUserService(email: string, password: string): Promise<Omit<IUser, 'password_hash'> & { token?: string }> {
-        if (!email || !password) {
-            throw new Error("Todos los datos son requeridos")
-        }
-
-        const existingEmail = await this.getUserForEmail(email)
-        if (!existingEmail) {
-            throw new Error("Usuario no encontrado")
-        }
-
-        const isValidPassword = await compare(password, existingEmail.password_hash)
-        if (!isValidPassword) {
-            throw new Error("Contraseña incorrecta")
-        }
-
-        const token = await jwtValidator.createJwt(existingEmail.id)
-        const { password_hash, ...userWithoutPassword } = existingEmail
-
-        return {
-            ...userWithoutPassword,
-            token
-        }
+    const existingEmail = await this.getUserForEmail(email);
+    if (!existingEmail) {
+      throw new Error("Usuario no encontrado");
     }
 
+    const isValidPassword = await compare(
+      password,
+      existingEmail.password_hash
+    );
+    if (!isValidPassword) {
+      throw new Error("Contraseña incorrecta");
+    }
 
-    async registerUserService(nombre: string, email: string, password: string, role = "ciudadano"): Promise<void> {
-        if (!nombre || !email || !password || !role) {
-            throw new Error("Todos los datos son requeridos")
-        }
+    const token = await jwtValidator.createJwt(existingEmail.id);
+    const { password_hash, ...userWithoutPassword } = existingEmail;
 
-        const existUser = await this.getUserForEmail(email)
-        if (existUser) {
-            throw new Error("El correo electrónico ya está en uso")
-        }
+    return {
+      ...userWithoutPassword,
+      token,
+    };
+  }
 
-        const roleResult = await pool.query('SELECT id FROM roles WHERE nombre = $1', [role]);
-        if (roleResult.rows.length === 0) {
-            throw new Error('Rol inválido');
-        }
+  async registerUserService(
+    nombre: string,
+    email: string,
+    password: string,
+    role = "supervisor"
+  ): Promise<void> {
+    if (!nombre || !email || !password || !role) {
+      throw new Error("Todos los datos son requeridos");
+    }
 
-        const roleId = roleResult.rows[0].id;
-        const salt = await genSalt(10);
-        const passwordHash = await hash(password, salt);
-        const userId = uuidv4();
-        await pool.query(`INSERT INTO users (id, nombre, email, password_hash, role_id)
+    const existUser = await this.getUserForEmail(email);
+    if (existUser) {
+      throw new Error("El correo electrónico ya está en uso");
+    }
+
+    console.log(existUser);
+
+    const roleResult = await pool.query(
+      "SELECT id FROM roles WHERE nombre = $1",
+      [role]
+    );
+    if (roleResult.rows.length === 0) {
+      throw new Error("Rol inválido");
+    }
+
+    const roleId = roleResult.rows[0].id;
+    const salt = await genSalt(10);
+    const passwordHash = await hash(password, salt);
+    const userId = uuidv4();
+    await pool.query(
+      `INSERT INTO users (id, nombre, email, password_hash, role_id)
             VALUES ($1, $2, $3, $4, $5)`,
-            [userId, nombre, email, passwordHash, roleId]
-        );
-        sendEvent('user_registered', { userId, nombre, email, role });
-    }
+      [userId, nombre, email, passwordHash, roleId]
+    );
+    sendEvent("user_registered", { userId, nombre, email, role });
+  }
 }
