771f09949c105a4adb98099ceab7ec8c6f7c99a4
Juan Q <juanqi3489@gmail.com>
2025-11-04 12:36:02 -0300

    Pasaron mas cosas

--- UserService/src/app.ts
+++ UserService/src/app.ts
@@ -9,7 +9,12 @@ import { authUserRoute } from './routes/userRoute'
 
 const app = express()
 
-app.use(cors())
+app.use(cors(
+    {
+        origin: "http://localhost:5173",
+        credentials: true
+    }
+))
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
